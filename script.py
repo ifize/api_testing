0ae11c364c8ca60a0a9143a69f198df4d8df1535
Ilya Voronkov <voronkovid@gmail.com>
2023-03-19 14:52:33 +0300

    version 2.0

--- script.py
+++ script.py
@@ -1,78 +1,63 @@
 import requests
 import time
 import csv
-import threading
-from typing import List
 
-# Установка интервала между запросами на создание отчета в 1 минуту
-interval = 60
+TOKEN = "1d706f2b-d4cf-43cc-8a38-0ad4a8a97887"
+API_URL = "https://analytics.maximum-auto.ru/vacancy-test/api/v0.1"
 
-# Заголовок Authorization
-headers = {'Authorization': 'Bearer 1d706f2b-d4cf-43cc-8a38-0ad4a8a97887'}
+def create_report(report_id):
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+    data = {"id": report_id}
+    response = requests.post(f"{API_URL}/reports", headers=headers, json=data)
+    if response.status_code == 201:
+        return True
+    elif response.status_code == 409:
+        return False
+    else:
+        raise Exception("Failed to create report")
 
-# URL для запросов
-url = 'https://analytics.maximum-auto.ru/vacancy-test/api/v0.1'
+def get_report(report_id):
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+    response = requests.get(f"{API_URL}/reports/{report_id}", headers=headers)
+    if response.status_code == 200:
+        return response.json()["value"]
+    elif response.status_code == 202:
+        return None
+    else:
+        raise Exception("Failed to get report")
 
-# Список для хранения идентификаторов отчетов и времени их создания
-reports = []
+def delete_report(report_id):
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+    response = requests.delete(f"{API_URL}/reports/{report_id}", headers=headers)
+    if response.status_code != 204:
+        raise Exception("Failed to delete report")
 
-# Функция для отправки запроса на создание отчета
-def create_report() -> None:
+def write_to_csv(date_time, value):
+    with open("results.csv", mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([date_time, value])
+
+while True:
+    # Создаем новый отчет
+    report_id = str(time.time())
+    while not create_report(report_id):
+        report_id = str(time.time())
+
+    # Запоминаем время создания отчета
+    report_time = time.time()
+
+    # Опрашиваем API до получения готового отчета
     while True:
-        # Отправка запроса на создание отчета
-        response = requests.post(url, headers=headers)
+        # Получаем текущее значение отчета
+        report_value = get_report(report_id)
+        if report_value is None:
+            time.sleep(1)
+            continue
 
-        # Обработка ответа со статусом 201
-        if response.status_code == 201:
-            # Добавление идентификатора отчета и времени его создания в список
-            reports.append((response.json()['report_id'], time.time()))
+        # Если отчет готов, записываем его в CSV-файл и удаляем его
+        write_to_csv(report_time, report_value)
+        delete_report(report_id)
+        break
 
-        # Задержка перед следующим запросом
-        time.sleep(interval)
-
-# Функция для получения отчета и записи его в файл
-def get_report(report_id: str, create_time: float) -> None:
-    while True:
-        # Отправка запроса на получение отчета
-        response = requests.get(f"{url}/{report_id}", headers=headers)
-
-        # Обработка ответа со статусом 200
-        if response.status_code == 200:
-            # Запись данных в файл
-            with open('results.csv', 'a', newline='') as file:
-                writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                writer.writerow([create_time, response.json()['value']])
-            break
-
-        # Задержка перед следующим запросом
-        time.sleep(1)
-
-# Открытие файла для записи результатов
-with open('results.csv', 'w', newline='') as file:
-    writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['date_time', 'value'])
-
-    # Запуск потока для отправки запросов на создание отчетов
-    t = threading.Thread(target=create_report)
-    t.start()
-
-    # Организация цикла для обработки готовых отчетов
-    while True:
-        for report in reports:
-            # Получение и удаление записи из списка
-            report_id, create_time = report
-            reports.remove(report)
-
-            # Запуск потока для получения отчета
-            t = threading.Thread(target=get_report, args=(report_id, create_time))
-            t.start()
-
-        # Задержка перед следующей итерацией
-        time.sleep(60) # Приостанавливаем выполнение на 60 секунд
-
-        # Проверка наличия новых отчетов
-        new_reports = get_report()
-
-        # Добавление новых отчетов в список
-        if new_reports:
-            reports.extend(new_reports)
+    # Ждем 1 минуту перед созданием нового отчета
+    time.sleep(60)
